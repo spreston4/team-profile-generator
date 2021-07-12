b018b94ec1a5dfab98ef8be4ddb9d5b349cbbdd4
spreston <sam.preston11@gmail.com>
2021-07-11 20:29:40 -0500

    Added shell / pseudocode for Employee Test.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,16 @@
 const index = require('../index');
+const employee = require('../lib/employee');
+
+describe("Employee", () => {
+
+    describe("Add Employee", () => {
+
+        it("Should XXXXXXXXXXXXXXXX"), () => {
+            // Create variables
+
+            // Action to take
+
+            // Expect
+        }
+    })
+})

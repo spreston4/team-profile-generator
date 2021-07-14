71214371f42bb3466daccd363f335df6b9952990
spreston <sam.preston11@gmail.com>
2021-07-14 16:57:08 -0500

    Functioning tests for Employee.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,56 @@
-const index = require('../index');
-const employee = require('../lib/employee');
+const Employee = require('../lib/employee');
 
-describe("Employee", () => {
+describe('Employee', () => {
 
-    describe("Add Employee", () => {
+    describe('Initialization', () => {
 
-        it("Should XXXXXXXXXXXXXXXX"), () => {
-            // Create variables
+        it('should create an employee object containing a name, id number and e-mail', () => {
+            
+            let sam = new Employee('Sam', 1, 'sam@email.com');
 
-            // Action to take
+            expect(sam.name).toEqual('Sam');
+            expect(sam.id).toEqual(1);
+            expect(sam.email).toEqual('sam@email.com');
+        });
+    });
 
-            // Expect
-        }
-    })
-})
+    describe('getName', () => {
+
+        it('should return the employee name', () => {
+            
+            let sam = new Employee('Sam', 1, 'sam@email.com');
+
+            expect(sam.getName()).toEqual('Sam');
+        });
+    });
+
+    describe('getId', () => {
+
+        it('should return the employee id number', () => {
+            
+            let sam = new Employee('Sam', 1, 'sam@email.com');
+
+            expect(sam.getId()).toEqual(1);
+        });
+    });
+
+    describe('getEmail', () => {
+
+        it('should return the employee email address', () => {
+            
+            let sam = new Employee('Sam', 1, 'sam@email.com');
+
+            expect(sam.getEmail()).toEqual('sam@email.com');
+        });
+    });
+
+    describe('getRole', () => {
+
+        it('should return the employee role name', () => {
+            
+            let sam = new Employee('Sam', 1, 'sam@email.com');
+
+            expect(sam.getRole()).toEqual('Employee');
+        });
+    });
+});

55a92cc7db5afaef9a586c9ccb80cda6b895bd46
spreston <sam.preston11@gmail.com>
2021-07-17 16:12:45 -0500

    Correctly generates HTML & CSS file from user input.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Packages
+// Requires packages & dependencies
 const fs = require('fs');
 const inquirer = require('inquirer');
 const questions = require('./lib/questions');
@@ -7,6 +7,7 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
+// Function 'askManager' is called on application load: prompts user for manager data from 'managerQuestions', saves response as a new Manager object in the employee array, calls 'askWhatsNext' to continue building team.
 const askManager = () => {
 
     inquirer
@@ -14,39 +15,39 @@ const askManager = () => {
 
         .then((mAns) => {
 
-            let newManager = new Manager(mAns.name, mAns.id, mAns.email, mAns.officeNumber);
-            render.employeeArr.push(newManager);
+            render.employeeArr.push(new Manager(mAns.name, mAns.id, mAns.email, mAns.officeNumber))
 
             askWhatsNext();
         })
 }
 
+// Function 'askEngineer' is called from 'askWhatsNext': prompts user for engineer data from 'engineerQuestions', saves response as a new Engineer object in the employee array, calls 'askWhatsNext' to continue building team.
 const askEngineer = () => {
     inquirer
         .prompt(questions.engineerQuestions)
 
         .then((eAns) => {
 
-            let newEngineer = new Engineer(eAns.name, eAns.id, eAns.email, eAns.github);
-            render.employeeArr.push(newEngineer);
-           
+            render.employeeArr.push(new Engineer(eAns.name, eAns.id, eAns.email, eAns.github));
+
             askWhatsNext();
         })
 }
 
+// Function 'askIntern' is called from 'askWhatsNext': prompts user for intern data from 'internQuestions', saves response as a new Intern object in the employee array, calls 'askWhatsNext' to continue building team.
 const askIntern = () => {
     inquirer
         .prompt(questions.internQuestions)
 
         .then((iAns) => {
-            
-            let newIntern = new Intern(iAns.name, iAns.id, iAns.email, iAns.school);
-            render.employeeArr.push(newIntern);
+
+            render.employeeArr.push(new Intern(iAns.name, iAns.id, iAns.email, iAns.school));
 
             askWhatsNext();
         })
 }
 
+// Function 'askwhatsNext' is called from all employee prompt functions: asks user what type of member they want to input next & calls the corresponding employee propt function; if user is done inputting team members, calls the 'renderPage' function to generate HTML and CSS.
 const askWhatsNext = () => {
 
     console.log('-------------------------------')
@@ -55,12 +56,12 @@ const askWhatsNext = () => {
         .prompt(questions.whatsNextQuestions)
 
         .then((whatNextAnswers) => {
-        
+
             if (whatNextAnswers.nextChoice === 'Engineer') askEngineer()
             else if (whatNextAnswers.nextChoice === 'Intern') askIntern()
             else render.renderPage();
         })
 }
 
-// -------------------------------- Initialize -------------------------------- //
+// Initialize application
 askManager();
